ec6c4bec4a126b61c10366a9449963e21d127899
Facu Carrion <facundojcarrion@gmail.com>
2026-09-23 12:24:43 -0300

    Agregar lista enlazada simple y UI en frmListaSimple
    Se implementa la clase clsListaSimple con métodos para gestionar nodos y su integración con controles de Windows Forms. Se añaden controles visuales y eventos en frmListaSimple para agregar y eliminar elementos, actualizando la interfaz. Se incorpora frmListaSimple.resx para recursos y se actualiza el proyecto para incluir los nuevos archivos.

---
+++ clsListaSimple.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace pryCarrionED
+{
+    internal class clsListaSimple
+    {
+        private clsNodo primero;
+
+        public clsNodo Primero { get => primero; set => primero = value; }
+
+        public void Agregar(clsNodo Nuevo)
+        {
+            if (Primero == null)
+            {
+                Primero = Nuevo;
+            }
+            else
+            {
+                if (Nuevo.Codigo <= Primero.Codigo)
+                {
+                    Nuevo.Siguiente = Primero;
+                    Primero = Nuevo;
+                }
+                else
+                {
+                    clsNodo aux = Primero;
+                    clsNodo ant = Primero;
+
+                    while (aux != null && Nuevo.Codigo > aux.Codigo)
+                    {
+                        ant = aux;
+                        aux = aux.Siguiente;
+                    }
+
+                    ant.Siguiente = Nuevo;
+                    Nuevo.Siguiente = aux;
+                }
+            }
+        }
+
+        public void Eliminar(Int32 Codigo)
+        {
+            if (Primero.Codigo == Codigo)
+            {
+                Primero = Primero.Siguiente;
+            }
+            else
+            {
+                clsNodo ant = Primero;
+                clsNodo aux = Primero;
+                while (aux.Codigo != Codigo)
+                {
+                    ant = aux;
+                    aux = aux.Siguiente;
+                }
+                ant.Siguiente = aux.Siguiente;
+            }
+        }
+
+
+
+        public void ListarEnGrilla(DataGridView dgvListado)
+        {
+            clsNodo aux = Primero;
+            dgvListado.Rows.Clear();
+            while (aux != null)
+            {
+                dgvListado.Rows.Add(aux.Codigo, aux.Nombre, aux.Tramite);
+                aux = aux.Siguiente;
+            }
+        }
+
+        public void Listar(ListBox lista)
+        {
+            clsNodo aux = Primero;
+            lista.Items.Clear();
+
+            while (aux != null)
+            {
+                lista.Items.Add(aux.Codigo + " " + aux.Nombre + " " + aux.Tramite);
+                aux = aux.Siguiente;
+            }
+        }
+
+        public void ListarEnCombo(ComboBox combo)
+        {
+            clsNodo aux = Primero;
+            combo.Items.Clear();
+            while (aux != null)
+            {
+                combo.Items.Add(aux.Codigo);
+                aux = aux.Siguiente;
+            }
+        }
+    }
+}
--- frmListaSimple.cs
+++ frmListaSimple.cs
@@ -16,5 +16,48 @@ namespace pryCarrionED
         {
             InitializeComponent();
         }
+
+        clsListaSimple objListaSimple = new clsListaSimple();
+
+        private void btnAgregar_Click(object sender, EventArgs e)
+        {
+            clsNodo Persona = new clsNodo();
+            Persona.Codigo = Convert.ToInt32(txtCodigo.Text);
+            Persona.Nombre = txtNombre.Text;
+            Persona.Tramite = txtTramite.Text;
+
+            objListaSimple.Agregar(Persona);
+
+            txtCodigo.Clear();
+            txtNombre.Clear();  
+            txtTramite.Clear();
+            cboCodigo.SelectedIndex = -1;
+
+            objListaSimple.Listar(lstListado);
+            objListaSimple.ListarEnGrilla(dgvListado);
+            objListaSimple.ListarEnCombo(cboCodigo);
+        }
+
+        private void btnEliminar_Click(object sender, EventArgs e)
+        {
+            if (cboCodigo.SelectedItem != null)
+            {
+                 objListaSimple.Eliminar(Convert.ToInt32(cboCodigo.SelectedItem));
+                 MessageBox.Show("Eliminado correctamente");
+            }
+            else
+            {
+                MessageBox.Show("No se ha seleccionado ningún valor a eliminar");
+            }
+
+            txtCodigo.Clear();
+            txtNombre.Clear();
+            txtTramite.Clear();
+            cboCodigo.SelectedIndex = -1;
+
+            objListaSimple.Listar(lstListado);
+            objListaSimple.ListarEnGrilla(dgvListado);
+            objListaSimple.ListarEnCombo(cboCodigo);
+        }
     }
 }
